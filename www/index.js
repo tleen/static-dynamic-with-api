98298e6cccd4dd16e587965e814ca634916618e8
Thomas Leen <tl@thomasleen.com>
2014-10-01 21:27:28 -0400

    added jquery

--- www/index.js
+++ www/index.js
@@ -1,3 +1,11 @@
-(function(){
-  console.log('hello world');
-})();
+(function($){
+  $('.btn-time').click(function(){
+    var btn = $(this);
+    $.get('http://api.staticdynamic.com/time',function(data){
+      $('#' + btn.attr('js-data-target')).text(data.time);
+    })
+      .fail(function(){
+	console.log('failed');
+      });
+  });
+})(jQuery);
